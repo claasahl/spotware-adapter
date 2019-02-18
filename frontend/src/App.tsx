b0fbd68cd87fcdccd66bdd216ca54be82dbf3a6c
Claas Ahlrichs <claasahl@gmail.com>
2019-02-18 18:33:35 +0100

    installed router-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,26 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMoneyBillAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 library.add(faMoneyBillAlt);
 
 import "./App.css";
+import Intro from "./Intro";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <FontAwesomeIcon icon={faMoneyBillAlt} size="4x" />
-          <h1 className="App-title">Connect Now</h1>
-          <h3>A Sample Application for Exploring Spotware's Open API v2</h3>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <FontAwesomeIcon icon={faMoneyBillAlt} size="4x" />
+            <h1 className="App-title">Connect Now</h1>
+            <h3>A Sample Application for Exploring Spotware's Open API v2</h3>
+          </header>
+          <Route to="/" component={Intro} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/src/Intro.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const Intro = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.tsx</code> and save to reload.
+  </p>
+);
+export default Intro;
