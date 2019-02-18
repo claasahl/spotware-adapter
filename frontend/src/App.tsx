35bc8cdafafd7e2bcc9778d54d2a6e6148f7359e
Claas Ahlrichs <claasahl@gmail.com>
2019-02-18 18:33:01 +0100

    updated app title and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import * as React from 'react';
-import './App.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMoneyBillAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from "react";
 
-import logo from './logo.svg';
+library.add(faMoneyBillAlt);
+
+import "./App.css";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <FontAwesomeIcon icon={faMoneyBillAlt} size="4x" />
+          <h1 className="App-title">Connect Now</h1>
+          <h3>A Sample Application for Exploring Spotware's Open API v2</h3>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
