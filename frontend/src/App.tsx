cf9309b86827a558e78f01b7bb9311d224a832c7
Claas Ahlrichs <claasahl@gmail.com>
2019-02-19 18:33:52 +0100

    installed apollo graphql client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,35 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMoneyBillAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ApolloClient from "apollo-boost";
 import * as React from "react";
+import { ApolloProvider } from "react-apollo";
 import { BrowserRouter, Route } from "react-router-dom";
-
-library.add(faMoneyBillAlt);
 
 import "./App.css";
 import Intro from "./Intro";
 
+library.add(faMoneyBillAlt);
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
+
 class App extends React.Component {
   public render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <FontAwesomeIcon icon={faMoneyBillAlt} size="4x" />
-            <h1 className="App-title">Connect Now</h1>
-            <h3>A Sample Application for Exploring Spotware's Open API v2</h3>
-          </header>
-          <Route to="/" component={Intro} />
-        </div>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div className="App">
+            <header className="App-header">
+              <FontAwesomeIcon icon={faMoneyBillAlt} size="4x" />
+              <h1 className="App-title">Connect Now</h1>
+              <h3>A Sample Application for Exploring Spotware's Open API v2</h3>
+            </header>
+            <Route to="/" component={Intro} />
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     );
   }
 }
