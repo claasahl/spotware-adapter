716892602c4aa674bab07bfe7626ba9a13042532
Claas Ahlrichs <claasahl@gmail.com>
2019-02-21 21:19:04 +0100

    https://www.apollographql.com/docs/react/advanced/boost-migration.html

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,9 +1,22 @@
-import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { withClientState } from "apollo-link-state";
+
+const cache = new InMemoryCache();
+
+const clientState = withClientState({
+  cache,
+  defaults: { isAuthenticated: false },
+  resolvers: {}
+});
+
+const httpLink = new HttpLink({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
 export const client = new ApolloClient({
-  clientState: {
-    defaults: { isAuthenticated: false },
-    resolvers: {}
-  },
-  uri: "https://48p1r2roz4.sse.codesandbox.io"
+  cache,
+  link: ApolloLink.from([clientState, httpLink])
 });
