48fe48d0f6c287a0b37bd17d6f203bda7440cc75
Claas Ahlrichs <claasahl@gmail.com>
2019-02-22 21:07:31 +0100

    extracted subscription into dedicated component with callback(s)

--- frontend/src/Protected.tsx
+++ frontend/src/Protected.tsx
@@ -1,26 +1,53 @@
 import * as React from "react";
-import { Subscription } from "react-apollo";
-import { INames, NAMES } from "./queries/names";
+import { OnSubscriptionDataOptions } from "react-apollo";
+import Names, { INames } from "./queries/names";
 
-export const Protected = () => (
-  <Subscription subscription={NAMES}>
-    {({ data, loading, error }) => {
-      if (loading) {
-        return "Loading...";
-      }
-      if (error) {
-        return `Error! ${error.message}`;
-      }
+interface IEvent {
+  id: string;
+  date: Date;
+}
+interface IState {
+  data: IEvent[];
+}
 
-      return (
-        <div>
-          <h3>Protected</h3>
-          <p>
-            {new Date().toISOString()}: {(data as INames).somethingChanged.id}
+export class Protected extends React.PureComponent<never, IState> {
+  public state: IState = {
+    data: []
+  };
+
+  public render() {
+    return (
+      <div>
+        <Names onEvent={this.onEvent} />
+        <h3>Protected</h3>
+        {!this.state.data.length && <p>no data, yet</p>}
+        {this.state.data.map(data => (
+          <p key={data.date.valueOf()}>
+            {data.date.toISOString()}: {data.id}
           </p>
-        </div>
-      );
-    }}
-  </Subscription>
-);
+        ))}
+      </div>
+    );
+  }
+
+  private onEvent = (event: OnSubscriptionDataOptions<INames>) => {
+    // const { data, error, loading } = event.subscriptionData;
+    const date = new Date();
+    const item = { id: JSON.stringify(event.subscriptionData), date };
+    this.setState(state => ({ data: [item, ...state.data] }));
+    // if (loading) {
+    //   const item = { id: "loading", date };
+    //   this.setState(state => ({ data: [item, ...state.data] }));
+    // } else if (error) {
+    //   const item = { id: `Error! ${error.message}`, date };
+    //   this.setState(state => ({ data: [item, ...state.data] }));
+    // } else if (data) {
+    //   const item = { id: data.somethingChanged.id, date };
+    //   this.setState(state => ({ data: [item, ...state.data] }));
+    // } else {
+    //   const item = { id: "?????", date };
+    //   this.setState(state => ({ data: [item, ...state.data] }));
+    // }
+  };
+}
 export default Protected;
