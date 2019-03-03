c19150e6095e141e36acb0943915e5cdbafdd3de
Claas Ahlrichs <claasahl@gmail.com>
2019-03-03 19:17:27 +0100

    implemented version request

--- backend/src/spotware/requests/ProtoOAVersionReq.ts
+++ backend/src/spotware/requests/ProtoOAVersionReq.ts
@@ -1,27 +1,13 @@
 import {
   ProtoOAVersionReq,
-  ProtoOAPayloadType,
   IProtoOAVersionReq,
-  IProtoMessage,
-  ProtoOAApplicationAuthRes
+  IProtoMessage
 } from "../../generated/spotware";
-
-// aliasing "ProtoOAApplicationAuthRes", because it only contains exactly one field "payloadType"
-type wrapper = typeof ProtoOAApplicationAuthRes;
-function toProtoMessage2<P>(
-  TYPE: wrapper,
-  properties: P,
-  clientMsgId: string | null | undefined
-): IProtoMessage {
-  const message = TYPE.create({ ...properties });
-  const payloadType = TYPE.prototype.payloadType;
-  const payload = TYPE.encode(message).finish();
-  return { payloadType, payload, clientMsgId };
-}
+import * as $base from "../message_handler";
 
 export function toProtoMessage(
   properties: IProtoOAVersionReq,
   clientMsgId: string | null | undefined
 ): IProtoMessage {
-  return toProtoMessage2(ProtoOAVersionReq, properties, clientMsgId);
+  return $base.toProtoMessage(ProtoOAVersionReq, properties, clientMsgId);
 }
--- backend/src/spotware/responses/ProtoOAVersionRes.ts
+++ backend/src/spotware/responses/ProtoOAVersionRes.ts
@@ -1,41 +1,14 @@
 import {
   ProtoOAVersionRes,
-  ProtoOAPayloadType,
-  ProtoPayloadType,
-  ProtoOAApplicationAuthRes,
-  IProtoMessage
+  ProtoOAPayloadType
 } from "../../generated/spotware";
 import { EventEmitter } from "events";
+import * as $base from "../message_handler";
 
-// aliasing "ProtoOAApplicationAuthRes", because it only contains exactly one field "payloadType"
-type wrapper = typeof ProtoOAApplicationAuthRes;
-
-function register2(
-  TYPE: wrapper,
-  EVENT: ProtoOAPayloadType | ProtoPayloadType,
-  emitter: EventEmitter
-): void {
-  emitter.on(EVENT.toString(), parseAndEmitMessage(TYPE, emitter));
-}
 export function register(emitter: EventEmitter): void {
-  register2(
+  $base.register(
     ProtoOAVersionRes,
     ProtoOAPayloadType.PROTO_OA_VERSION_RES,
     emitter
   );
-}
-
-function parseAndEmitMessage(
-  TYPE: wrapper,
-  emitter: EventEmitter
-): (message: IProtoMessage) => void {
-  return (message: IProtoMessage) => {
-    if (message.payload) {
-      const msg = TYPE.decode(message.payload);
-      if (msg) {
-        emitter.emit(TYPE.name, msg);
-        emitter.emit("message", msg);
-      }
-    }
-  };
 }
