5be5d27c6a20f8db98cbd12bb9f558beb28427b6
Claas Ahlrichs <claasahl@gmail.com>
2019-03-03 19:15:42 +0100

    log incoming ProtoMessages to console

--- backend/src/spotware/session.ts
+++ backend/src/spotware/session.ts
@@ -4,6 +4,7 @@ import { Response } from "./responses/ProtoOAVersionRes";
 
 export function create(socket: TLSSocket): Gateway {
   const gateway = new Gateway(socket);
+  gateway.emitter.on("message", (data: any) => console.log("-->", data));
   registerResponseHandlers(gateway);
   return gateway;
 }
