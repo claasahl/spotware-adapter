113d98ead198e367610605d18be3e09bdc4b4b19
Claas Ahlrichs <claasahl@gmail.com>
2019-03-04 16:11:08 +0100

    generated factory functions for spotware responses

--- backend/src/spotware/responses.ts
+++ backend/src/spotware/responses.ts
@@ -2,10 +2,173 @@ import * as $spotware from "../generated/spotware";
 import { EventEmitter } from "events";
 import * as $base from "./message_handler";
 
+export function ProtoOAApplicationAuthRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAApplicationAuthRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_APPLICATION_AUTH_RES,
+    emitter
+  );
+}
+export function ProtoOAAccountAuthRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAAccountAuthRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_ACCOUNT_AUTH_RES,
+    emitter
+  );
+}
+export function ProtoOAErrorRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAErrorRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_ERROR_RES,
+    emitter
+  );
+}
 export function ProtoOAVersionRes(emitter: EventEmitter): void {
   $base.register(
     $spotware.ProtoOAVersionRes,
     $spotware.ProtoOAPayloadType.PROTO_OA_VERSION_RES,
+    emitter
+  );
+}
+export function ProtoOAAssetListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAAssetListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_ASSET_LIST_RES,
+    emitter
+  );
+}
+export function ProtoOASymbolsListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASymbolsListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SYMBOLS_LIST_RES,
+    emitter
+  );
+}
+export function ProtoOASymbolByIdRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASymbolByIdRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SYMBOL_BY_ID_RES,
+    emitter
+  );
+}
+export function ProtoOASymbolsForConversionRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASymbolsForConversionRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SYMBOLS_FOR_CONVERSION_RES,
+    emitter
+  );
+}
+export function ProtoOAAssetClassListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAAssetClassListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_ASSET_CLASS_LIST_RES,
+    emitter
+  );
+}
+export function ProtoOATraderRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOATraderRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_TRADER_RES,
+    emitter
+  );
+}
+export function ProtoOAReconcileRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAReconcileRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_RECONCILE_RES,
+    emitter
+  );
+}
+export function ProtoOADealListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOADealListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_DEAL_LIST_RES,
+    emitter
+  );
+}
+export function ProtoOAExpectedMarginRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAExpectedMarginRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_EXPECTED_MARGIN_RES,
+    emitter
+  );
+}
+export function ProtoOACashFlowHistoryListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOACashFlowHistoryListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_CASH_FLOW_HISTORY_LIST_RES,
+    emitter
+  );
+}
+export function ProtoOAGetAccountListByAccessTokenRes(
+  emitter: EventEmitter
+): void {
+  $base.register(
+    $spotware.ProtoOAGetAccountListByAccessTokenRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_RES,
+    emitter
+  );
+}
+export function ProtoOASubscribeSpotsRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASubscribeSpotsRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SUBSCRIBE_SPOTS_RES,
+    emitter
+  );
+}
+export function ProtoOAUnsubscribeSpotsRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAUnsubscribeSpotsRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_UNSUBSCRIBE_SPOTS_RES,
+    emitter
+  );
+}
+export function ProtoOAGetTrendbarsRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAGetTrendbarsRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_GET_TRENDBARS_RES,
+    emitter
+  );
+}
+export function ProtoOAGetTickDataRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAGetTickDataRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_GET_TICKDATA_RES,
+    emitter
+  );
+}
+export function ProtoOAGetCtidProfileByTokenRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAGetCtidProfileByTokenRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_GET_CTID_PROFILE_BY_TOKEN_RES,
+    emitter
+  );
+}
+export function ProtoOASubscribeDepthQuotesRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASubscribeDepthQuotesRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SUBSCRIBE_DEPTH_QUOTES_RES,
+    emitter
+  );
+}
+export function ProtoOAUnsubscribeDepthQuotesRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAUnsubscribeDepthQuotesRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_UNSUBSCRIBE_DEPTH_QUOTES_RES,
+    emitter
+  );
+}
+export function ProtoOASymbolCategoryListRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOASymbolCategoryListRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_SYMBOL_CATEGORY_RES,
+    emitter
+  );
+}
+export function ProtoOAAccountLogoutRes(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.ProtoOAAccountLogoutRes,
+    $spotware.ProtoOAPayloadType.PROTO_OA_ACCOUNT_LOGOUT_RES,
     emitter
   );
 }
--- backend/src/test.ts
+++ backend/src/test.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
 import schema from "protocol-buffers-schema";
+import { Message } from "protocol-buffers-schema/types";
 
-function generateRequest(type: string): string {
-  const generated = `export function ${type}(
+function generateRequest(message: Message): string {
+  const type = message.name;
+  return `export function ${type}(
   properties: $spotware.I${type},
   clientMsgId: string | null | undefined
 ): $spotware.IProtoMessage {
@@ -12,7 +14,21 @@ function generateRequest(type: string): string {
     clientMsgId
   );
 }`;
-  return generated;
+}
+
+function generateResponse(message: Message) {
+  const payloadTypeField = message.fields.filter(
+    field => field.name === "payloadType"
+  )[0];
+  const type = message.name;
+  const payloadType = payloadTypeField.options["default"];
+  return `export function ${type}(emitter: EventEmitter): void {
+  $base.register(
+    $spotware.${type},
+    $spotware.ProtoOAPayloadType.${payloadType},
+    emitter
+  );
+}`;
 }
 
 function writeRequests(requests: string[]) {
@@ -29,14 +45,35 @@ function writeRequests(requests: string[]) {
   }
 }
 
+function writeResponses(responses: string[]) {
+  if (responses.length > 0) {
+    const stream = fs.createWriteStream("src/spotware/responses.ts");
+    stream.write('import * as $spotware from "../generated/spotware";\n');
+    stream.write('import { EventEmitter } from "events";\n');
+    stream.write('import * as $base from "./message_handler";\n');
+    stream.write("\n");
+    for (const response of responses) {
+      stream.write(response);
+    }
+    stream.write("\n");
+    stream.end();
+  }
+}
+
 const protoFile =
   "node_modules/@claasahl/spotware-protobuf-messages/OpenApiMessages.proto";
 const proto = fs.readFileSync(protoFile);
 const s = schema(proto);
 const requests: string[] = [];
+const responses: string[] = [];
 for (const message of s.messages) {
   if (message.name.endsWith("Req")) {
-    requests.push(generateRequest(message.name));
+    requests.push(generateRequest(message));
+  } else if (message.name.endsWith("Res")) {
+    responses.push(generateResponse(message));
+  } else {
+    console.log("not categorized:", message.name);
   }
 }
 writeRequests(requests);
+writeResponses(responses);
