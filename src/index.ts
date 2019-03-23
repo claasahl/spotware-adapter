8bc32898f979b9c92da946ed55ce981a440a37e2
Claas Ahlrichs <claasahl@gmail.com>
2019-03-23 14:17:28 +0100

    drafted index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { SpotwareSocket } from "./spotware-socket";
+
+export * from "./spotware-messages";
+export * from "./spotware-socket";
+export * from "./spotware-utils";
+export default SpotwareSocket;
