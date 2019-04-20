921d857d9d86ba56c3198600d5f8e1527aa956e0
Claas Ahlrichs <claasahl@gmail.com>
2019-04-20 18:27:47 +0200

    consolidated SpotwareEventEmitter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./spotware-client";
 export { SpotwareEventEmitter } from "./spotware-event-emitter";
-export { SpotwareEvents } from "./spotware-events";
 export * from "./spotware-messages";
--- src/spotware-writer.ts
+++ src/spotware-writer.ts
@@ -1,50 +1,11 @@
 import tls from "tls";
-import { EventEmitter } from "events";
 
 import * as $spotware from "./spotware-messages";
 import * as messages from "./messages";
 import * as util from "./spotware-utils";
+import SpotwareEventEmitter from "./spotware-event-emitter";
 
 type SpotwareClient = tls.TLSSocket & SpotwareEventEmitter;
-
-interface SpotwareEventEmitter extends EventEmitter {
-  addListener(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  on(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  once(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  prependListener(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  prependOnceListener(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  removeListener(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  off(
-    event: "PROTO_MESSAGE",
-    listener: (message: $spotware.IProtoMessage) => void
-  ): this;
-  removeAllListeners(event?: "PROTO_MESSAGE"): this;
-  setMaxListeners(n: number): this;
-  getMaxListeners(): number;
-  listeners(event: "PROTO_MESSAGE"): Function[];
-  rawListeners(event: "PROTO_MESSAGE"): Function[];
-  emit(event: "PROTO_MESSAGE", message: $spotware.IProtoMessage): boolean;
-  eventNames(): Array<string | symbol>;
-  listenerCount(type: "PROTO_MESSAGE"): number;
-}
 
 function readProtoMessage(this: SpotwareClient, data: string) {
   {
