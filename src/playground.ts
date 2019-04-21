177d12761d5a8cf8150070ed5d2be606a9f7c1e6
Claas Ahlrichs <claasahl@gmail.com>
2019-04-21 12:11:08 +0200

    updated playground.ts

--- src/playground.ts
+++ src/playground.ts
@@ -1,11 +1,13 @@
 import * as $spotware from "./spotware-messages";
-import * as messages from "./messages";
+import { to, from } from "./messages";
 import { writeProtoMessage, connect } from "./spotware-client";
 
 const client = connect(
   5035,
   "live.ctraderapi.com"
 );
+
+// handle (incoming) proto messages
 client.on("PROTO_MESSAGE", message => {
   console.log(
     $spotware.ProtoPayloadType[message.payloadType] ||
@@ -13,24 +15,27 @@ client.on("PROTO_MESSAGE", message => {
   );
   switch (message.payloadType) {
     case $spotware.ProtoPayloadType.ERROR_RES: {
-      const msg = messages.from("ERROR_RES", message);
+      const msg = from("ERROR_RES", message);
       console.log(msg);
       break;
     }
     case $spotware.ProtoOAPayloadType.PROTO_OA_VERSION_REQ: {
-      const msg = messages.from("PROTO_OA_VERSION_REQ", message);
+      const msg = from("PROTO_OA_VERSION_REQ", message);
       console.log(msg);
       break;
     }
     case $spotware.ProtoOAPayloadType.PROTO_OA_VERSION_RES: {
-      const msg = messages.from("PROTO_OA_VERSION_RES", message);
+      const msg = from("PROTO_OA_VERSION_RES", message);
       console.log(msg);
       break;
     }
   }
 });
+
+// write (outgoing) proto messages
 setInterval(() => {
-  const message = messages.to("HEARTBEAT_EVENT", {});
+  const message = to("HEARTBEAT_EVENT", {});
   writeProtoMessage(client, message);
 }, 10000);
-writeProtoMessage(client, messages.to("PROTO_OA_VERSION_REQ", {}));
+
+writeProtoMessage(client, to("PROTO_OA_VERSION_REQ", {}));
