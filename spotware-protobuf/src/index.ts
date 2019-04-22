88a0ce05abe8a210de20ef989667c1136c1d949a
Claas Ahlrichs <claasahl@gmail.com>
2019-04-22 12:42:08 +0200

    export .json files

---
+++ spotware-protobuf/src/index.ts
@@ -0,0 +1,12 @@
+import commonMessages from "../assets/OpenApiCommonMessages.json";
+import commonModelMessages from "../assets/OpenApiCommonModelMessages.json";
+import messages from "../assets/OpenApiMessages.json";
+import modelMessages from "../assets/OpenApiModelMessages.json";
+
+export const types = {
+  commonMessages,
+  commonModelMessages,
+  messages,
+  modelMessages
+};
+export default types;
