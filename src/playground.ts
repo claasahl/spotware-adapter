3599332cbf5f1e5e991c95a75ee9734f23bf9c63
Claas Ahlrichs <claasahl@gmail.com>
2020-09-27 14:10:50 +0200

    simplified access to factory methods

--- src/playground.ts
+++ src/playground.ts
@@ -1,4 +1,4 @@
-import { SpotwareClientStream, ProtoOAPayloadType } from ".";
+import { SpotwareClientStream, ProtoOAPayloadType, FACTORY } from ".";
 
 const config = {
   host: process.env.SPOTWARE__HOST || "live.ctraderapi.com",
@@ -9,27 +9,33 @@ const config = {
 };
 
 const s = new SpotwareClientStream(config.port, config.host);
-setInterval(() => s.HEARTBEAT_EVENT(), 10000);
+setInterval(() => s.write(FACTORY.HEARTBEAT_EVENT()), 10000);
 s.on("data", (msg) => {
   switch (msg.payloadType) {
     case ProtoOAPayloadType.PROTO_OA_VERSION_RES:
       console.log("version: ", msg.payload.version);
-      s.OA_APPLICATION_AUTH_REQ({
-        clientId: config.clientId,
-        clientSecret: config.clientSecret,
-      });
+      s.write(
+        FACTORY.PROTO_OA_APPLICATION_AUTH_REQ({
+          clientId: config.clientId,
+          clientSecret: config.clientSecret,
+        })
+      );
       break;
     case ProtoOAPayloadType.PROTO_OA_APPLICATION_AUTH_RES:
-      s.OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_REQ({
-        accessToken: config.accessToken,
-      });
+      s.write(
+        FACTORY.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_REQ({
+          accessToken: config.accessToken,
+        })
+      );
       break;
     case ProtoOAPayloadType.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_RES:
       msg.payload.ctidTraderAccount.forEach((account) => {
-        s.OA_ACCOUNT_AUTH_REQ({
-          accessToken: config.accessToken,
-          ctidTraderAccountId: account.ctidTraderAccountId,
-        });
+        s.write(
+          FACTORY.PROTO_OA_ACCOUNT_AUTH_REQ({
+            accessToken: config.accessToken,
+            ctidTraderAccountId: account.ctidTraderAccountId,
+          })
+        );
       });
       break;
     case ProtoOAPayloadType.PROTO_OA_ACCOUNT_AUTH_RES:
@@ -37,4 +43,4 @@ s.on("data", (msg) => {
       break;
   }
 });
-s.OA_VERSION_REQ();
+s.write(FACTORY.PROTO_OA_VERSION_REQ());
