5a855469c5ac15e4a8ea0f1adf0f65ed7ae62b98
Claas Ahlrichs <claasahl@gmail.com>
2025-10-02 22:19:00 +0200

    let's go with this for now

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,30 @@
 import { Messages } from "./messages";
 
-export type MessageHandler<Context> = (
-  message: Messages,
+// Generic handler that accepts any message from the union
+export type MessageHandler<Context, Message extends Messages = Messages> = (
+  message: Message,
   ctx: Context,
 ) =>
   | ReadonlyArray<Messages>
   | Promise<ReadonlyArray<Messages>>;
 
+// Extract specific message type by payloadType
+type MessageByPayloadType<T extends Messages["payloadType"]> = Extract<
+  Messages,
+  { payloadType: T }
+>;
+
 export class Router<Context> {
   private handlers = new Map<Messages["payloadType"], MessageHandler<Context>>();
 
-  register<T extends Messages>(
-    type: T["payloadType"],
-    handler: MessageHandler<Context>,
+  register<T extends Messages["payloadType"]>(
+    type: T,
+    handler: MessageHandler<Context, MessageByPayloadType<T>>,
   ) {
-    this.handlers.set(type, handler);
+    // TypeScript doesn't understand that a handler for a specific message type
+    // is compatible with a handler for the general Messages union due to
+    // contravariance. We need to help it understand this safe assignment.
+    this.handlers.set(type, handler as MessageHandler<Context>);
   }
 
   async handle(
