35ebc0083f8a14232f39d8fb214d18cef1ebadd2
Claas Ahlrichs <claasahl@gmail.com>
2025-10-03 21:02:56 +0200

    outlined requests to access candles

--- src/examples/client.ts
+++ src/examples/client.ts
@@ -1,11 +1,18 @@
 import tls from "tls";
+import { randomUUID } from "crypto";
+import { setTimeout } from "timers/promises";
 
-import { ProtoOaPayloadType, FACTORY } from "..";
+import {
+  ProtoOaPayloadType,
+  FACTORY,
+  ProtoOATrendbarPeriod,
+  ProtoPayloadType,
+} from "..";
 import { Protocol } from "../protocol";
 import { Router } from "../router";
 
-const host = "demo.ctraderapi.com";
-const port = 5035;
+const host = process.env.SPOTWARE__HOST || "";
+const port = parseInt(process.env.SPOTWARE__PORT || "", 10);
 
 const socket = tls.connect(port, host);
 socket.once("secureConnect", () => {
@@ -15,11 +22,88 @@ socket.once("secureConnect", () => {
 
 const protocol = new Protocol(socket);
 const router = new Router<undefined>();
-router.register(ProtoOaPayloadType.PROTO_OA_VERSION_RES, (message) => {
-  console.log(`got response from ${host}:${port}`);
-  console.log(`server version: ${message.payload.version}`);
-  protocol.close();
-  return [];
+router.register(ProtoPayloadType.HEARTBEAT_EVENT, () => {
+  return [FACTORY.HEARTBEAT_EVENT({})];
+});
+setInterval(() => protocol.send(FACTORY.HEARTBEAT_EVENT({})), 10000)
+router.register(ProtoOaPayloadType.PROTO_OA_VERSION_RES, () => {
+  return [
+    FACTORY.PROTO_OA_APPLICATION_AUTH_REQ(
+      {
+        clientId: process.env.SPOTWARE__CLIENT_ID || "",
+        clientSecret: process.env.SPOTWARE__CLIENT_SECRET || "",
+      },
+      randomUUID(),
+    ),
+  ];
+});
+router.register(ProtoOaPayloadType.PROTO_OA_APPLICATION_AUTH_RES, () => {
+  return [
+    FACTORY.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_REQ(
+      {
+        accessToken: process.env.SPOTWARE__ACCESS_TOKEN || "",
+      },
+      randomUUID(),
+    ),
+  ];
+});
+router.register(
+  ProtoOaPayloadType.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_RES,
+  (msg) => {
+    return msg.payload.ctidTraderAccount.map(({ ctidTraderAccountId }) =>
+      FACTORY.PROTO_OA_ACCOUNT_AUTH_REQ({
+        accessToken: process.env.SPOTWARE__ACCESS_TOKEN || "",
+        ctidTraderAccountId,
+      }),
+    );
+  },
+);
+router.register(ProtoOaPayloadType.PROTO_OA_ACCOUNT_AUTH_RES, (msg) => {
+  return [
+    FACTORY.PROTO_OA_SYMBOLS_LIST_REQ(
+      {
+        ctidTraderAccountId: msg.payload.ctidTraderAccountId,
+      },
+      randomUUID(),
+    ),
+  ];
+});
+router.register(ProtoOaPayloadType.PROTO_OA_SYMBOLS_LIST_RES, (msg) => {
+  const eurusd = msg.payload.symbol.find(
+    ({ symbolName }) => symbolName?.toUpperCase() === "EURUSD",
+  );
+  if (!eurusd) {
+    protocol.close();
+    return [];
+  }
+  router.register(ProtoOaPayloadType.PROTO_OA_SUBSCRIBE_SPOTS_RES, (msg) => {
+    return [
+      FACTORY.PROTO_OA_SUBSCRIBE_LIVE_TRENDBAR_REQ(
+        {
+          ctidTraderAccountId: msg.payload.ctidTraderAccountId,
+          period: ProtoOATrendbarPeriod.M1,
+          symbolId: eurusd.symbolId,
+        },
+        randomUUID(),
+      ),
+    ];
+  });
+  router.register(
+    ProtoOaPayloadType.PROTO_OA_SUBSCRIBE_LIVE_TRENDBAR_RES,
+    () => {
+      return [];
+    },
+  );
+
+  return [
+    FACTORY.PROTO_OA_SUBSCRIBE_SPOTS_REQ(
+      {
+        ctidTraderAccountId: msg.payload.ctidTraderAccountId,
+        symbolId: [eurusd.symbolId],
+      },
+      randomUUID(),
+    ),
+  ];
 });
 
 (async () => {
@@ -27,5 +111,8 @@ router.register(ProtoOaPayloadType.PROTO_OA_VERSION_RES, (message) => {
   for await (const message of protocol.messages()) {
     const replies = await router.handle(message, undefined);
     protocol.send(...replies);
+
+    // throttling
+    await setTimeout(200);
   }
 })();
